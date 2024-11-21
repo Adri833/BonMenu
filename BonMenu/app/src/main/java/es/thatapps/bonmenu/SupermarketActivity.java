64ecf089fdb74a5b1934ba2d5cc8aa0f34f52cb5
Adrián Álvarez <adrialvarez638@gmail.com>
2024-11-21 13:22:17 +0100

    Minor changes

--- BonMenu/app/src/main/java/es/thatapps/bonmenu/SupermarketActivity.java
+++ BonMenu/app/src/main/java/es/thatapps/bonmenu/SupermarketActivity.java
@@ -1,6 +1,7 @@
 package es.thatapps.bonmenu;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,19 +21,21 @@ public class SupermarketActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_supermarket);
 
+        ImageButton backButton = findViewById(R.id.backButton); // Boton para ir hacia atras
+        backButton.setOnClickListener(v -> finish());
+
         // Referencia al RecyclerView
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
 
         // Configura el LayoutManager
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
         // Lista de supermercados
         List<Supermercado> supermercados = new ArrayList<>();
-        supermercados.add(new Supermercado("Mercadona", "https://drive.google.com/uc?export=download&id=1lZBOyaL9YP4_2OX77ss3E4qDOuT6hHJB"));
-        supermercados.add(new Supermercado("Ese", "https://drive.google.com/uc?export=download&id=1lZBOyaL9YP4_2OX77ss3E4qDOuT6hHJB"));
-        supermercados.add(new Supermercado("Puto", "https://drive.google.com/uc?export=download&id=1lZBOyaL9YP4_2OX77ss3E4qDOuT6hHJB"));
-        supermercados.add(new Supermercado("Tito", "https://drive.google.com/uc?export=download&id=1lZBOyaL9YP4_2OX77ss3E4qDOuT6hHJB"));
-        supermercados.add(new Supermercado("Berto", "https://drive.google.com/uc?export=download&id=1lZBOyaL9YP4_2OX77ss3E4qDOuT6hHJB"));
+        supermercados.add(new Supermercado("Mercadona", "https://drive.google.com/uc?export=download&id=1vNdzHruoGWO2qqqT3sMoPqIiTmZRR93S"));
+        supermercados.add(new Supermercado("Lidl", "https://drive.google.com/uc?export=download&id=1380U_QDiZNi7NkiYJdBaYQI9WqkdGqv_"));
+        supermercados.add(new Supermercado("Carrefour", "https://drive.google.com/uc?export=download&id=161sfgzM24yXv9wOcw8rDbIMGjXieUwma"));
+        supermercados.add(new Supermercado("Dia", "https://drive.google.com/uc?export=download&id=1M5pf8Sm_S935gaPWZaaAI_IsM7FDrC81"));
+        supermercados.add(new Supermercado("Alcampo", "https://drive.google.com/uc?export=download&id=1mWlstkXrQvfUumA5ecQ_MnSM0QxqB8Bb"));
 
         // Crea el adaptador y pasa la lista de supermercados al adaptador
         SupermarketAdapter adapter = new SupermarketAdapter(supermercados, this);

77540046fe2339c8ab9340ff4f6b7e5657b84403
Adri833 <adrialvarez638@gmail.com>
2024-11-25 14:40:44 +0100

    Navigation started

--- BonMenu/app/src/main/java/es/thatapps/bonmenu/HomeActivity.java
+++ BonMenu/app/src/main/java/es/thatapps/bonmenu/HomeActivity.java
@@ -8,6 +8,8 @@ import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +34,20 @@ public class HomeActivity extends AppCompatActivity {
         // Botones de los elementos
         ImageButton supermarketButton = findViewById(R.id.supermarketButton);
         supermarketButton.setOnClickListener(v -> openSupermarketActivity());
-        // Botón de configuraciones
 
-
-
+        // Navegacion inferior
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.nav_home) {
+                openHomeActivity();
+                return true;
+            } else if (id == R.id.nav_settings) {
+                openSettingsActivity();
+                return true;
+            }
+            return false;
+        });
     }
 
     // Navegaciones
@@ -44,4 +56,13 @@ public class HomeActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void openHomeActivity() {
+        Intent intent = new Intent(this, HomeActivity.class); // Navegacion hacia home
+        startActivity(intent);
+    }
+
+    public void openSettingsActivity() {
+        Intent intent = new Intent(this, SettingsActivity.class); // Navegacion hacia settings
+        startActivity(intent);
+    }
 }
